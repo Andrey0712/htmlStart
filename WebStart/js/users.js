86e88d6fbf33b02e3ae8d4f2519f1419deb2ed50
nashemistoart@gmail.com <nashemistoart@gmail.com>
2021-07-07 15:39:29 +0300

    dell

--- WebStart/js/users.js
+++ WebStart/js/users.js
@@ -9,6 +9,7 @@
     var btnAddNewUser = document.getElementById("btnAddNewUser");
     var btnAddUserSave = document.getElementById("btnAddUserSave");
     var btnDell = document.getElementById("btnDell");
+    var f = '<input type="checkbox" id="complete" value="no">';
 
     var tbodyUsers = document.getElementById("tbodyUsers");
 
@@ -33,14 +34,8 @@
                             <td>${lastName}</td>
                             <td>${name}</td>
                             <td>${phone}</td>
-                            <td>
-                           <i class="fa fa-times fa-2x text-danger cursor-pointer" aria-hidden="true"></i>
-                           <i class="fa fa-times fa-2x text-danger cursor-pointer" aria-hidden="true"></i>
-                             
+                            <td>${f}</td>
                             
-        
-
-    </td>
                         `;
 
         txtLastName.value = txtName.value = txtPhone.value = "";
@@ -48,18 +43,66 @@
           // $(this).closest("tr").remove();
         tbodyUsers.appendChild(tr);
 
-       
+        
     };
 
 
-    btnDell.onclick = function deleteRow() {
-       
-        ell = checkboxElement.closest("tr"); // tr element (ваша строчка)
-        ell.parentElement.removeChild(ell); // удаляем всю строку
-      };
+    function DeleteRow(e) {
+        var tbodyUsers = document.getElementById('tbodyUsers');
+
+        bootbox.confirm("Ви точно хочете видалити об'єкт?", function (result) {
+            if (result) {
+                tbodyUsers.removeChild(e.parentElement.parentElement);
+            }
+        });
+    }
+
+
+    //       checkbox.addEventListener("click", function (e) {
+    //       checkboxElement = e.target; // элемент который вызвал функцию
+            
+    //    });
+    //btnDell.onclick =  function () {
+           
+    //       ell = checkboxElement.closest("tr"); // tr element (ваша строчка)
+    //    ell.parentElement.removeChild(ell); // удаляем всю строку
+
+    //    });
+
+        
+
+
+
+
+    }
+
+
+
+
+    //btnDell.onclick = function (tbodyUsers){
+
+    //    for (var rowi = tbodyUsers.rows.length; rowi-- > 0;) {
+    //        var row = tbodyUsers.rows[rowi];
+    //        var inputs = row.getElementsByTagName('input');
+    //        for (var inputi = inputs.length; inputi-- > 0;) {
+    //            var input = inputs[inputi];
+
+    //            if (input.type === 'checkbox' && input.checked) {
+    //                row.parentNode.removeChild(row);
+    //                break;
+    //            }
+    //        }
+    //    }
+    //  };
+
+
+
 
     //<button class="btnEdit"><i class="fa fa-pencil fa-2x text-info cursor-pointer" aria-hidden="true"></i></button>
     //<button class="btnDell"><i class="fa fa-times fa-2x text-danger cursor-pointer" aria-hidden="true"></i></button>   
-    
 
-}
+    //<td>
+    //    <i class="fa fa-times fa-2x text-danger cursor-pointer" aria-hidden="true"></i>
+    //    <i class="fa fa-times fa-2x text-danger cursor-pointer" aria-hidden="true"></i>
+    //</td>
+    
