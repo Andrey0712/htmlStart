c7b0b94367e67782ae4187c08bb91e084e227472
nashemistoart@gmail.com <nashemistoart@gmail.com>
2021-07-10 12:08:50 +0300

    dell

--- WebStart/js/users.js
+++ WebStart/js/users.js
@@ -8,8 +8,8 @@
 
     var btnAddNewUser = document.getElementById("btnAddNewUser");
     var btnAddUserSave = document.getElementById("btnAddUserSave");
-    var btnDell = document.getElementById("btnDell");
-    var f = '<input type="checkbox" id="complete" value="no">';
+    
+    //var f = '<input type="checkbox" id="complete" value="no">';
 
     var tbodyUsers = document.getElementById("tbodyUsers");
 
@@ -34,10 +34,12 @@
                             <td>${lastName}</td>
                             <td>${name}</td>
                             <td>${phone}</td>
-                            <td>${f}</td>
                             
-<button class="btnEdit"><i class="fa fa-pencil fa-2x text-info cursor-pointer" aria-hidden="true"></i></button>
-    <button class="btnDell"><i class="fa fa-times fa-2x text-danger cursor-pointer" aria-hidden="true"></i></button>
+                            
+                            <td>
+                              <i class="fa fa-pencil fa-2x text-info cursor-pointer" aria-hidden="true"></i>
+                              <i class="fa fa-times fa-2x text-danger cursor-pointer" aria-hidden="true" onclick="DeleteRow(this)"></i>
+                            </td>
 
                         `;
 
@@ -49,17 +51,7 @@
         
     };
 
-
-    btnDell.onclick= function DeleteRow(e) {
-        var tbodyUsers = document.getElementById('tbodyUsers');
-
-        bootbox.confirm("Ви точно хочете видалити об'єкт?", function (result) {
-            if (result) {
-                tbodyUsers.removeChild(e.parentElement.parentElement);
-            }
-        });
-    }
-
+     
 
     //       checkbox.addEventListener("click", function (e) {
     //       checkboxElement = e.target; // элемент который вызвал функцию
@@ -72,14 +64,19 @@
 
     //    });
 
-        
-
-
 
 
     }
 
+function DeleteRow(e) {
+    var tbodyUsers = document.getElementById('tbodyUsers');
 
+    bootbox.confirm("Ви точно хочете видалити об'єкт?", function (result) {
+        if (result) {
+            tbodyUsers.removeChild(e.parentElement.parentElement);
+        }
+    });
+}
 
 
     //btnDell.onclick = function (tbodyUsers){
@@ -104,8 +101,5 @@
     //<button class="btnEdit"><i class="fa fa-pencil fa-2x text-info cursor-pointer" aria-hidden="true"></i></button>
     //<button class="btnDell"><i class="fa fa-times fa-2x text-danger cursor-pointer" aria-hidden="true"></i></button>   
 
-    //<td>
-    //    <i class="fa fa-times fa-2x text-danger cursor-pointer" aria-hidden="true"></i>
-    //    <i class="fa fa-times fa-2x text-danger cursor-pointer" aria-hidden="true"></i>
-    //</td>
+    
     
